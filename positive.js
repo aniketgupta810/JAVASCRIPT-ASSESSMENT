b3724b8b9a504157a1398369b87bdde591422d7a
aniketgupta810 <98165185+aniketgupta810@users.noreply.github.com>
2022-01-29 18:05:41 +0530

    Add files via upload

---
+++ positive.js
@@ -0,0 +1,11 @@
+const number = 10;
+if (number > 0) {
+    console.log("The number is positive");
+}
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative");
+}
